cf224d2f38f92f05b943fc6eebc5b2412eff9500
Prateekp16 <47525892+Prateekp16@users.noreply.github.com>
2019-09-24 16:01:18 +0530

    Add project files.

---
+++ WebApplication/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var Movie = new movie()
+                {
+                Name = "This Shit is real and it works "      
+                };
+           
+            return View(Movie);
+            //return new ViewResult();
+            //return Content("Hello WEB!");
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
+            }
+
+        public ActionResult Edit(int movieId)
+            {
+            return Content("id=" + movieId);
+            }
+    }
+}
